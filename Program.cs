e4c74544917e863e938a8a3cab28975b9fddcb10
David Maltsev <luterfaeron@gmail.com>
2021-08-02 19:58:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedactorOfArrayHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

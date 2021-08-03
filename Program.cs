f32248229730d4d7061702517b144fb731e291b7
David Maltsev <luterfaeron@gmail.com>
2021-08-03 18:20:45 +0300

    Добавлена перегрузка метода AddToArray принимающие значение элемента в виде строки.

--- Program.cs
+++ Program.cs
@@ -107,6 +107,59 @@ namespace AddToArray
 
             return ref arrayToChange;
         }
+
+        /// <summary>
+        /// Метод добавляет элемент в массив с заданным индексом. Значение принимаются в типе string.
+        /// </summary>
+        /// <param name="arrayToChange"></param>
+        /// <param name="indexOfElementSting"></param>
+        /// <param name="valueOfElementString"></param>
+        /// <returns></returns>
+        static ref int[] AddToArray(ref int[] arrayToChange, uint indexOfElement, string valueOfElementString) 
+        {
+            int valueOfElement;
+
+            bool succesInParsingValue = true;
+
+            if (succesInParsingValue != int.TryParse(valueOfElementString, out valueOfElement))
+            {
+                Console.WriteLine("Вы ввели некорректное значение для элемента массива. Допустимо только челое число. Массив не был обработан.");
+                return ref arrayToChange;
+            }
+
+            if (arrayToChange.Length < indexOfElement)
+            {
+                Console.WriteLine("Заданный вами индекс находится за границами допустимого значения, массив не был обработан. Для данного массива максимальный допустимый индекс: " + arrayToChange.Length);
+                return ref arrayToChange;
+            }
+
+            int[] arrayForSavingData = new int[arrayToChange.Length];
+            uint newLengthOfArray = Convert.ToUInt32(arrayToChange.Length) + 1;
+
+            for (int currentElement = 0; currentElement < arrayToChange.Length; currentElement++)
+            {
+                arrayForSavingData[currentElement] = arrayToChange[currentElement];
+            }
+
+            arrayToChange = new int[newLengthOfArray];
+
+            for (uint currentElement = 0, elementOfArrayToOuput = 0; currentElement < arrayForSavingData.Length; currentElement++, elementOfArrayToOuput++)
+            {
+                if (indexOfElement == currentElement)
+                {
+                    arrayToChange[indexOfElement] = valueOfElement;
+                    elementOfArrayToOuput++;
+                }
+                else if (indexOfElement == arrayForSavingData.Length)
+                {
+                    arrayToChange[indexOfElement] = valueOfElement;
+                }
+                arrayToChange[elementOfArrayToOuput] = arrayForSavingData[currentElement];
+
+            }
+
+            return ref arrayToChange;
+        }
         static void Main()
         {
         }

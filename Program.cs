1968dd48b6e1d8ebb390d41109658ee01180e8e7
David Maltsev <luterfaeron@gmail.com>
2021-08-03 18:06:13 +0300

    Было исправлено название метода на "AddToArray" для наглядности.  Была добавлена перегрузка для обработки индекса и значяения в виде строки.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 /*
  * Написать метод для добавления эелемента в начало массива, в конец массива и по указаному индексу.
  */
-namespace RedactorOfArrayHW
+namespace AddToArray
 {
     class Program
     {
@@ -10,11 +10,11 @@ namespace RedactorOfArrayHW
         /// Метод добавляет элемент в массив с заданным индексом.
         /// </summary>
         /// <param name="arrayToChange"></param>
-        /// <param name="indexOfElementToChange"></param>
+        /// <param name="indexOfElement"></param>
         /// <returns></returns>
-        static ref int[] RedactorOfArray(ref int[] arrayToChange, uint indexOfElementToChange, int valueOfElement)
+        static ref int[] AddToArray(ref int[] arrayToChange, uint indexOfElement, int valueOfElement)
         {
-            if (arrayToChange.Length < indexOfElementToChange)
+            if (arrayToChange.Length < indexOfElement)
             {
                 Console.WriteLine("Заданный вами индекс находится за границами допустимого значения, массив не был обработан. Для данного массива максимальный допустимый индекс: " + arrayToChange.Length);
                 return ref arrayToChange;
@@ -32,14 +32,74 @@ namespace RedactorOfArrayHW
 
             for (uint currentElement = 0, elementOfArrayToOuput = 0; currentElement < arrayForSavingData.Length; currentElement++, elementOfArrayToOuput++)
             {
-                if (indexOfElementToChange == currentElement)
+                if (indexOfElement == currentElement)
                 {
-                    arrayToChange[indexOfElementToChange] = valueOfElement;
+                    arrayToChange[indexOfElement] = valueOfElement;
                     elementOfArrayToOuput++;
                 }
-                else if (indexOfElementToChange == arrayForSavingData.Length) 
+                else if (indexOfElement == arrayForSavingData.Length) 
                 {
-                    arrayToChange[indexOfElementToChange] = valueOfElement;
+                    arrayToChange[indexOfElement] = valueOfElement;
+                }
+                arrayToChange[elementOfArrayToOuput] = arrayForSavingData[currentElement];
+
+            }
+
+            return ref arrayToChange;
+        }
+
+        /// <summary>
+        /// Метод добавляет элемент в массив с заданным индексом. Индекс и значение принимаются в типе string.
+        /// </summary>
+        /// <param name="arrayToChange"></param>
+        /// <param name="indexOfElementToChange"></param>
+        /// <param name="valueOfElement"></param>
+        /// <returns></returns>
+        static ref int[] AddToArray(ref int[] arrayToChange, string indexOfElementSting, string valueOfElementString) 
+        {
+            uint indexOfElement;
+            int valueOfElement;
+
+            bool succesInParsingIndex = true, succesInParsingValue = true;
+
+            if (succesInParsingIndex != uint.TryParse(indexOfElementSting, out indexOfElement))
+            {
+                Console.WriteLine("Вы ввели некорректное значение для индекса. Допустимо только целое число без знака. Массив не был обработан.");
+                return ref arrayToChange;
+            }
+
+            if (succesInParsingValue != int.TryParse(valueOfElementString, out valueOfElement))
+            {
+                Console.WriteLine("Вы ввели некорректное значение для элемента массива. Допустимо только челое число. Массив не был обработан.");
+                return ref arrayToChange;
+            }
+
+            if (arrayToChange.Length < indexOfElement)
+            {
+                Console.WriteLine("Заданный вами индекс находится за границами допустимого значения, массив не был обработан. Для данного массива максимальный допустимый индекс: " + arrayToChange.Length);
+                return ref arrayToChange;
+            }
+
+            int[] arrayForSavingData = new int[arrayToChange.Length];
+            uint newLengthOfArray = Convert.ToUInt32(arrayToChange.Length) + 1;
+
+            for (int currentElement = 0; currentElement < arrayToChange.Length; currentElement++)
+            {
+                arrayForSavingData[currentElement] = arrayToChange[currentElement];
+            }
+
+            arrayToChange = new int[newLengthOfArray];
+
+            for (uint currentElement = 0, elementOfArrayToOuput = 0; currentElement < arrayForSavingData.Length; currentElement++, elementOfArrayToOuput++)
+            {
+                if (indexOfElement == currentElement)
+                {
+                    arrayToChange[indexOfElement] = valueOfElement;
+                    elementOfArrayToOuput++;
+                }
+                else if (indexOfElement == arrayForSavingData.Length)
+                {
+                    arrayToChange[indexOfElement] = valueOfElement;
                 }
                 arrayToChange[elementOfArrayToOuput] = arrayForSavingData[currentElement];
 
@@ -49,7 +109,6 @@ namespace RedactorOfArrayHW
         }
         static void Main()
         {
-
         }
     }
 }

9ac2572677f582846d2ab429ad1d9b1abc33a9e8
David Maltsev <luterfaeron@gmail.com>
2021-08-03 19:21:00 +0300

    Добавлена перегрузка метода, которая принимает булево значение определяющие в начало или в конец вставлять элемент массива.

--- Program.cs
+++ Program.cs
@@ -55,14 +55,14 @@ namespace AddToArray
         /// <param name="indexOfElementToChange"></param>
         /// <param name="valueOfElement"></param>
         /// <returns></returns>
-        static ref int[] AddToArray(ref int[] arrayToChange, string indexOfElementSting, string valueOfElementString) 
+        static ref int[] AddToArray(ref int[] arrayToChange, string indexOfElementString, string valueOfElementString) 
         {
             uint indexOfElement;
             int valueOfElement;
 
             bool succesInParsingIndex = true, succesInParsingValue = true;
 
-            if (succesInParsingIndex != uint.TryParse(indexOfElementSting, out indexOfElement))
+            if (succesInParsingIndex != uint.TryParse(indexOfElementString, out indexOfElement))
             {
                 Console.WriteLine("Вы ввели некорректное значение для индекса. Допустимо только целое число без знака. Массив не был обработан.");
                 return ref arrayToChange;
@@ -109,7 +109,7 @@ namespace AddToArray
         }
 
         /// <summary>
-        /// Метод добавляет элемент в массив с заданным индексом. Значение принимаются в типе string.
+        /// Метод добавляет элемент в массив с заданным индексом. Значение принимается в типе string.
         /// </summary>
         /// <param name="arrayToChange"></param>
         /// <param name="indexOfElementSting"></param>
@@ -160,8 +160,101 @@ namespace AddToArray
 
             return ref arrayToChange;
         }
+
+        /// <summary>
+        /// Метод добавляет элемент в массив с заданным индексом. Индекс принимается в типе string
+        /// </summary>
+        /// <param name="arrayToChange"></param>
+        /// <param name="indexOfElementString"></param>
+        /// <param name="valueOfElement"></param>
+        /// <returns></returns>
+        static ref int[] AddToArray(ref int[] arrayToChange, string indexOfElementString, int valueOfElement) 
+        {
+            uint indexOfElement;
+
+            bool succesInParsingIndex = true;
+
+            if (succesInParsingIndex != uint.TryParse(indexOfElementString, out indexOfElement))
+            {
+                Console.WriteLine("Вы ввели некорректное значение для индекса. Допустимо только целое число без знака. Массив не был обработан.");
+                return ref arrayToChange;
+            }
+
+            if (arrayToChange.Length < indexOfElement)
+            {
+                Console.WriteLine("Заданный вами индекс находится за границами допустимого значения, массив не был обработан. Для данного массива максимальный допустимый индекс: " + arrayToChange.Length);
+                return ref arrayToChange;
+            }
+
+            int[] arrayForSavingData = new int[arrayToChange.Length];
+            uint newLengthOfArray = Convert.ToUInt32(arrayToChange.Length) + 1;
+
+            for (int currentElement = 0; currentElement < arrayToChange.Length; currentElement++)
+            {
+                arrayForSavingData[currentElement] = arrayToChange[currentElement];
+            }
+
+            arrayToChange = new int[newLengthOfArray];
+
+            for (uint currentElement = 0, elementOfArrayToOuput = 0; currentElement < arrayForSavingData.Length; currentElement++, elementOfArrayToOuput++)
+            {
+                if (indexOfElement == currentElement)
+                {
+                    arrayToChange[indexOfElement] = valueOfElement;
+                    elementOfArrayToOuput++;
+                }
+                else if (indexOfElement == arrayForSavingData.Length)
+                {
+                    arrayToChange[indexOfElement] = valueOfElement;
+                }
+                arrayToChange[elementOfArrayToOuput] = arrayForSavingData[currentElement];
+
+            }
+
+            return ref arrayToChange;
+        }
+
+        /// <summary>
+        /// Метод добавляет элемент в начало или конец массива. toBeFirst = true - в начало, toBeFirst = false в конец.
+        /// </summary>
+        /// <param name="arrayToChange"></param>
+        /// <param name="toBeFirst"></param>
+        /// <param name="valueOfElement"></param>
+        /// <returns></returns>
+        static ref int[] AddToArray(ref int[] arrayToChange, bool toBeFirst, int valueOfElement) 
+        {
+            uint indexOfElement = toBeFirst ? 0 : Convert.ToUInt32((arrayToChange.Length));
+
+            int[] arrayForSavingData = new int[arrayToChange.Length];
+            uint newLengthOfArray = Convert.ToUInt32(arrayToChange.Length) + 1;
+
+            for (int currentElement = 0; currentElement < arrayToChange.Length; currentElement++)
+            {
+                arrayForSavingData[currentElement] = arrayToChange[currentElement];
+            }
+
+            arrayToChange = new int[newLengthOfArray];
+
+            for (uint currentElement = 0, elementOfArrayToOuput = 0; currentElement < arrayForSavingData.Length; currentElement++, elementOfArrayToOuput++)
+            {
+                if (indexOfElement == currentElement)
+                {
+                    arrayToChange[indexOfElement] = valueOfElement;
+                    elementOfArrayToOuput++;
+                }
+                else if (indexOfElement == arrayForSavingData.Length)
+                {
+                    arrayToChange[indexOfElement] = valueOfElement;
+                }
+                arrayToChange[elementOfArrayToOuput] = arrayForSavingData[currentElement];
+
+            }
+
+            return ref arrayToChange;
+        }
         static void Main()
         {
+            
         }
     }
 }
